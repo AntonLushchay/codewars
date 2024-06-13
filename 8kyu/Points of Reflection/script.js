1ba134995dd35ca69ce5fa230f2884de4e01ee31
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-13 02:21:22 +0200

    second kata

--- 8kyu/Points of Reflection/script.js
+++ 8kyu/Points of Reflection/script.js
@@ -1,10 +1,15 @@
+/*Discription
+
+"Point reflection" or "point symmetry" is a basic concept in geometry where a given point, P, at a given position relative to a mid-point, Q has a corresponding point, P1, which is the same distance from Q but in the opposite direction.
+
+Task
+Given two points P and Q, output the symmetric point of point P about Q. Each argument is a two-element array of integers representing the point's X and Y coordinates. Output should be in the same format, giving the X and Y coordinates of point P1. You do not have to validate the input.
+*/
+
 function symmetricPoint(p, q) {
     let p1 = [];
     let diffX = Math.abs(q[0] - p[0]);
     let diffY = Math.abs(q[1] - p[1]);
-
-    console.log(diffX);
-    console.log(diffY);
 
     if (q[0] < p[0]) {
         p1[0] = q[0] - diffX;
@@ -21,4 +26,4 @@ function symmetricPoint(p, q) {
     return p1;
 }
 
-console.log(symmetricPoint([-3, 4], [-2, -6]));
+console.log('coordinates of point "p1": ', symmetricPoint([-3, 4], [-2, -6]));
