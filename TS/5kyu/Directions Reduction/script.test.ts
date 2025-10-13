86056ff63b92a5dd0e02041b990f17c99f5433af
AntonLushchay <av.lushchay17@gmail.com>
2025-10-13 20:55:11 +0200

    +1 5kyu. Total: 97

---
+++ TS/5kyu/Directions Reduction/script.test.ts
@@ -0,0 +1,14 @@
+//import {dirReduc} from './solution';
+//import chai, {assert} from "chai";
+//chai.config.truncateThreshold = 0;
+import { assert } from 'chai';
+import { dirReduc } from './script';
+
+describe('Fixed Tests', function () {
+    it('dirReduc', function () {
+        assert.deepEqual(dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST']), [
+            'WEST',
+        ]);
+        assert.deepEqual(dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH']), []);
+    });
+});
