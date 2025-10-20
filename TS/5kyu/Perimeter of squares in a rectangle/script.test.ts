c5c828d71887d531bcb1baf0ee4bb3200d3fafa9
AntonLushchay <av.lushchay17@gmail.com>
2025-10-20 22:52:19 +0200

    +1 5kyu. Total: 102

---
+++ TS/5kyu/Perimeter of squares in a rectangle/script.test.ts
@@ -0,0 +1,15 @@
+import { perimeter } from './script';
+import { assert } from 'chai';
+
+function testing(n: number, expected: number) {
+    assert.strictEqual(perimeter(n), expected);
+}
+
+describe('Fixed Tests', function () {
+    it('Basic tests perimeter', function () {
+        testing(5, 80);
+        testing(7, 216);
+        testing(20, 114624);
+        testing(30, 14098308);
+    });
+});
